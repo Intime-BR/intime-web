83c97414a5799ab166168e7de4ce5f962cd26f1c
barbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2022-09-11 13:00:33 -0300

    ajustes filtros

--- intime-web/src/pages/ActiveRoom/activeRoom.tsx
+++ intime-web/src/pages/ActiveRoom/activeRoom.tsx
@@ -56,16 +56,16 @@ export function ActiveRoom() {
         className="container-fluid justify-self-center mt-3 mb-3"
       >
         <div className="row p-3">
-          <div className="col-3">
+          <div className="col-sm-12 col-lg-3 col-md-6 mt-3 mb-3">
             <SearchSelect placeHolder="Selecione a Matrícula" />
           </div>
-          <div className="col-3">
+          <div className="col-sm-12 col-lg-3 col-md-6  mt-3 mb-3">
             <SearchSelect placeHolder="Selecione o Status" />
           </div>
-          <div className="col-3">
+          <div className="col-sm-12 col-lg-3 col-md-6  mt-3 mb-3">
             <SearchSelect placeHolder="Selecione a Turma" />
           </div>
-          <div className="col-3">
+          <div className="col-sm-12 col-lg-3 col-md-6  mt-3 mb-3">
             <SearchSelect placeHolder="Selecione a Disciplina" />
           </div>
         </div>
