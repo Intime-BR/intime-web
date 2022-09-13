a38cae6b6f5add485c1d79b8f9737e493fd433f8
barbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2022-09-12 22:39:48 -0300

    padronização de temas

--- intime-web/src/theme.ts
+++ intime-web/src/theme.ts
@@ -1,7 +1,25 @@
 export const theme = {
   colors: {
-    primary: `rgba(100,112,232)`,
+    primary: `#6470E8`,
+
+    purpleDark:"#17192E",
+
     lightPrimary: `rgba(39,52,182, 0.6)`,
+
     primaryVariant: "#b62763",
+
+    buttonGray: "#B7B7B7",
+
+    pending:"#EBAA02",
+
+    danger:"rgba(255,0,0, 0.6)",
+    
+    success:"#4A9231",
+    
+    background: "#F8F8F8",
+
+    white: "#FFFFFF",
+
+    sideBarItem:"#5689EC"
   },
 };
