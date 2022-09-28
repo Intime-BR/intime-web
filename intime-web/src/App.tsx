c62fd82dffa52b802b621ba73deceb8d2d21d00d
barbaraMAlmeida <barbara.nicollealmeida@gmail.com>
2022-09-28 18:18:13 -0300

    finalização crud

--- intime-web/src/App.tsx
+++ intime-web/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "antd/dist/antd.css";
 import AppRoutes from "./routes";
 import "./App.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return <AppRoutes />;
--- intime-web/src/services/registerUserService.ts
+++ intime-web/src/services/registerUserService.ts
@@ -20,12 +20,20 @@ export const createUser = (user: User) =>
     data: new Date(),
   });
 
-export const updateUser = (user: User) => {
-  Api.post<{ user: User }>("/users/update"),
-    {
-      nome: user.nome,
-      email: user.email,
-      senha: user.senha,
-      data: new Date(),
-    };
+export const updateUser = (user: User, hashId: any) => {
+  console.log({
+    nome: user.nome,
+    email: user.email,
+    senha: user.senha,
+  });
+
+  Api.put<{ user: User }>(`/users/update/${hashId}`, {
+    nome: user.nome,
+    email: user.email,
+    senha: user.senha,
+  });
+};
+
+export const deleteUser = (hashId: any) => {
+  Api.delete<{ user: User }>(`/users/delete/${hashId}`);
 };
